00127e9e41503d4f828f4da3a383240444b87073
Farhan Paal <farhan.12345.pala@gmail.com>
2025-06-25 18:27:45 +0000

    added prime

---
+++ pythonCode/22_primeOrNot.py
@@ -0,0 +1,30 @@
+"""
+WAP that checks weather a number is prime or not
+"""
+
+from math import sqrt
+def prime():
+  num = int(input("Enter number: "))
+
+  if num <= 1:
+    print("Not prime")
+    return
+
+  if num == 2:
+    print("It is prime")
+    return
+
+  sqr = int(sqrt(num))
+  is_prime = True
+
+  for i in range(2, sqr + 1):
+    if num % i == 0:
+      is_prime = False
+      break
+
+  if is_prime:
+    print("It is prime")
+  else:
+    print("Not prime")
+
+prime()
