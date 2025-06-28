a7cc35a0530fb57f9b68810d52abf44645396aff
Farhan Paal <farhan.12345.pala@gmail.com>
2025-06-28 07:27:15 +0000

    added txt file

---
+++ pythonCode/29_largest-number-List.py
@@ -0,0 +1,14 @@
+"""
+This program finds largest number in a list and prints that
+"""
+
+def find():
+  numbers=[66,99,668,9,88]
+  print(max(numbers))
+  store=0
+  for i in numbers:
+    if i>store:
+      i=store
+  print(store)
+
+find()
