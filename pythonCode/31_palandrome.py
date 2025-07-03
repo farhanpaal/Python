86c35ea364c4c329cc9194c10db39a1cc1f12917
Farhan Paal <farhan.12345.pala@gmail.com>
2025-07-03 12:33:25 +0000

    added

--- pythonCode/31_palandrome.py
+++ pythonCode/31_palandrome.py
@@ -3,48 +3,54 @@ This program takes a string and returns True if the string is palandrome otherwi
 
 eg of palandrome: level, madam, 12321
 """
-
-
 """---------Methiod 1---------"""
+
+
 def palandrome():
-  userInput=input("Enter a string: ") #asbbsa
-  strLength= len(userInput)
-  flag=True
+  userInput = input("Enter a string: ")  #asbbsa
+  strLength = len(userInput)
+  flag = True
   for i in range(strLength // 2):
-    if userInput[i] != userInput[strLength - 1 -i]: #a != a
-      flag=False
+    if userInput[i] != userInput[strLength - 1 - i]:  #a != a
+      flag = False
       break
   if flag:
     print("Palandrome")
   else:
     print("Not a palandrome")
+
+
 # palandrome()
-
-
 """---------Methiod 2---------"""
+
+
 def palandrome2():
-  userInput=input("Enter a string: ") #asbbsa
-  strLength= len(userInput)
-  count =0
-  lg=strLength-1
+  userInput = input("Enter a string: ")  #asbbsa
+  strLength = len(userInput)
+  count = 0
+  lg = strLength - 1
   for i in range(lg):
     if userInput[i] == userInput[lg]:
-      count=count+1
-      lg=lg-1
-  lg=len(userInput)
-  if count == lg-1:
+      count = count + 1
+      lg = lg - 1
+  lg = len(userInput)
+  if count == lg - 1:
     print("This is a palandrome")
   else:
     print("Not a palandrome")
+
+
 # palandrome2()
-
-
 """---------Methiod 3---------"""
+
+
 def palandrome3():
-  name=input("Enter a string: ")
-  revName=name[::-1]
-  if name==revName:
+  name = input("Enter a string: ")
+  revName = name[::-1]
+  if name == revName:
     print("This is palandrome")
   else:
     print("This is not a palandrome")
+
+
 palandrome3()
