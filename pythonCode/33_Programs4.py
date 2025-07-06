78a9d0930cd070037c0f238be1c04b1f901e40a5
Farhan Paal <farhan.12345.pala@gmail.com>
2025-07-06 18:59:59 +0000

    added program

---
+++ pythonCode/33_Programs4.py
@@ -0,0 +1,60 @@
+"""
+This program takes odd numbers from a list and stores them in other list and then prints 
+"""
+
+def oddNum():
+  lst=[1,2,3,4,5,6,7,8,99,65,78,5]
+  oddList=[]
+  for i in lst:
+    if i%2!=0:
+      oddList.append(i)
+
+  print(oddList)
+# oddNum()
+
+
+
+"""
+this program prints the total number of words in a string
+"""
+def wordCount():
+  string="farhan is my name"
+  elem= string.split()
+  print(len(elem))
+# wordCount()
+
+
+
+
+"""
+# this program filters out the element which starts with any specific alphabet
+"""
+def startWith():
+  string=["pala","and","shahid","ahsaan","rubait"]
+  store=[]
+  for i in string:
+    if i.lower().startswith("a"):
+      store.append(i)
+  print(store)
+
+startWith()
+
+
+
+
+"""
+This program prints out if the year user enters is leap or not
+"""
+def leap():
+  try:
+    year = int(input("Enter the year"))
+  except:
+    raise Exception("Wrong input")
+  else:
+    if year / 4 == 0:
+      print(f"{year} is a leap year")
+    else:
+      print(f"{year} is not a leap year")
+
+
+leap()
--- pythonCode/prac.py
+++
@@ -1,9 +0,0 @@
-"""
-This program takes a list of numbers and then separates from that the elements which are divisible by 3
-"""
-
-nums=[33,45,4,6,7,99,9,5]
-mul=[]
-for i in nums:
-  if i%3==0: mul.append(i)
-print(mul)
