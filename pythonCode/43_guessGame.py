51a12f1cc4506a05202a2c4e1559ebc120989c9b
Farhan Paal <farhan.12345.pala@gmail.com>
2025-07-25 16:41:27 +0000

    added

--- pythonCode/43_guessGame.py
+++ pythonCode/43_guessGame.py
@@ -1,4 +1,32 @@
+"""
+This is a guessing name built by using while loop
+"""
+import random as rand
+
+
 def guess():
-  print("Enter number to guess (0-10)")
+  randomNum = rand.randint(0, 10)
+  while True:
+    try:
+      num = int(input("Enter number to guess (0-10): "))
+    except:
+      print("Wrong input")
+
+    else:
+      if num == randomNum:
+        print("You win🎉🎉🎇🎆")
+        break  # Changed from exit() to break
+      else:
+        if randomNum > num:
+          print("Incorrect: Your entered number too low")
+        else:
+          print("Incorrect: Your entered number too high ")
+        continue
+
+  # This code runs after break (but wouldn't run after exit())
+  print("Thanks for playing!")
+  print("Game ended.")
+
 
 guess()
+print("Program finished.")  # This also runs with break, but not with exit()
