09a5fec6978e453d79e62f10bd1926d774740b43
Farhan Paal <farhan.12345.pala@gmail.com>
2025-07-27 05:40:51 +0000

    added

--- pythonCode/42_frequencyElem.py
+++ pythonCode/42_frequencyElem.py
@@ -8,4 +8,24 @@ def freq():
   while i<len(lstSet):
     print(f"{lstSet[i]} is printed {lst.count(lstSet[i])} times.")
     i+=1
-freq()
+# freq()
+
+
+
+def freq2():
+  lst=[3,4,6,2,1,3,4,2,3,6,3,2]
+  dictLst={}
+
+
+  print(dictLst)
+  index=0
+  while index< len(lst):
+    if lst[index] in dictLst: 
+      dictLst[index]=dictLst[index]+1
+    else:
+      dictLst[index]=1
+    index+=1
+
+  print(dictLst)
+
+freq2()
