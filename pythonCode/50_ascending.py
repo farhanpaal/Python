4ece3a34ccad294516344f2d8ae9a890b616ecf0
FARHAN PALA <farhan.12345.pala@gmail.com>
2025-09-10 13:21:21 +0530

    added

--- pythonCode/50_ascending.py
+++ pythonCode/50_ascending.py
@@ -1,13 +1,17 @@
-class Asc:
+class Asc():
     def __init__(self,stack):
         self.stack=stack
     def sort(self):
         tmp=[]
         while len(self.stack)!=0:
             top=self.stack.pop()
-            while len(tmp) !=0 and tmp[-1]>top:
+            while len(tmp)!=0 and top<tmp[-1]:
                 self.stack.append(tmp.pop())
             tmp.append(top)
         return tmp
-asc=Asc([12,22,34])
+
+
+asc=Asc([34,66,76,3,23])
 print(asc.sort())
+
+
