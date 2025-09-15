e8a08fb05d691dd016eb18fd39dd4d2655f71342
FARHAN PALA <farhan.12345.pala@gmail.com>
2025-09-15 12:19:45 +0530

    added

---
+++ uniProjects/10_oops.py
@@ -0,0 +1,23 @@
+class Customer:
+    def __init__(self, name, mobile, address):
+        self.__name=name
+        self.mobile=mobile
+        self.address=address
+
+    #setter
+    def setterFunc(self,newName):
+        self.__name=newName
+
+    # getter
+    def getterFunc(self):
+        print(self.__name)
+
+    def __str__(self): #if we need string representation, python itself will call this.
+        return f"Mr. {self.__name} is from {self.address}. Contact him on {self.mobile}"
+    
+ 
+
+cust= Customer("Pala",1234,"kashmir")
+cust.setterFunc("Farhan ")
+print(cust)
+cust.getterFunc()
