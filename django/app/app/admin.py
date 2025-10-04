4a2204a8b7b93f0c252c63f806caa8b3fea8366b
FARHAN PALA <farhan.12345.pala@gmail.com>
2025-10-04 15:13:50 +0530

    added

--- django/app/app/admin.py
+++ django/app/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin 
 from .models import Enquiry #enquiry is model name
-admin.site.register(Enquiry)
+
+
+#customize view in database
+class EnquiryAdmin(admin.ModelAdmin):  #we will destructure modelAdmin here
+    list_display=("name","email","message")  #this variable is in admin
+
+    # again regester (modal, customize view)
+admin.site.register(Enquiry, EnquiryAdmin)
