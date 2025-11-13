f660fc4682c1ba550efda996ca38c8017630b4d2
Farhan Paal <farhan.12345.pala@gmail.com>
2025-11-14 01:28:27 +0530

    Update 20-validParenthesis.py

--- leetProblems/20-validParenthesis.py
+++ leetProblems/20-validParenthesis.py
@@ -19,3 +19,31 @@ class Solution(object):
 solution = Solution()
 s = "()[]{}"  
 print(solution.isValid(s))
+
+
+
+# Another way to solve this question using dictionary
+
+class Solution2(object):
+    def isValid(self, s):
+        stack = []
+        pairs = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in pairs.values():
+                stack.append(char)
+            elif char in pairs.keys():
+                if not stack or stack[-1] != pairs[char]:
+                    return False
+                stack.pop()
+            else:
+                return False
+
+        return len(stack) == 0
+
+ 
+solution2 = Solution2()
+
+ 
+s = input("Enter a string of brackets (e.g. '()[]{}'): ")
+print("Valid:" if solution2.isValid(s) else "Invalid!")
