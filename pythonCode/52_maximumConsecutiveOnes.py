0bbb2e0c07954b3fd849d67ede44b3a207a7db19
Farhan Paal <farhan.12345.pala@gmail.com>
2025-11-18 00:54:52 +0530

    Update 52_maximumConsecutiveOnes.py

--- pythonCode/52_maximumConsecutiveOnes.py
+++ pythonCode/52_maximumConsecutiveOnes.py
@@ -1,1 +1,20 @@
+class Solution():
+    def findMaxConsecutiveOnes(self, nums):
+        
+        max_seq = 0  
+        seq = 0       
+
+        for num in nums:
+            if num == 1:
+                seq += 1
+                if seq > max_seq:
+                    max_seq = seq
+            else:
+                seq = 0  
+
+        return max_seq
+
+
+solution = Solution()
+print(solution.findMaxConsecutiveOnes([0,1,0,0,1,1,1]))
 
